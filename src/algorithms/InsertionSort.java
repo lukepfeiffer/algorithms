b7c3b7f809321f6f8cd85070e37492ad06dd60b1
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-05-29 15:10:52 -0400

    Insertion sort

--- src/algorithms/InsertionSort.java
+++ src/algorithms/InsertionSort.java
@@ -1,10 +1,30 @@
 package algorithms;
 
 public class InsertionSort extends ParentAlgorithms {
-	public static void main(String args[] ){
+	public static void main(String args[]) {
+		int arraySize = 20;
+		int[] array = ParentAlgorithms.createRandomArray(arraySize-1);
+		
+		System.out.println("Unsorted: " + ParentAlgorithms.arrayToString(array));
+
+		sort(array);
+		
+		System.out.println("Sorted: " + ParentAlgorithms.arrayToString(array));
 	}
-	
-	public static int[] sort(int [] array){
-		return null;
+
+	public static int[] sort(int[] array) {
+		int i; // the number of items sorted so far
+		int key; // the item to be inserted
+		int j;
+
+		for (i = 1; i < array.length; i++){
+		    for (j = i; j > 0 && array[j-1] > array[j]; j--){
+		        key = array[j];
+		        array[j] = array[j-1];
+		        array[j-1] = key;
+		    }
+		}
+		
+		return array;
 	}
 }
--- src/algorithms/InsertionSortTest.java
+++ src/algorithms/InsertionSortTest.java
@@ -12,6 +12,11 @@ public class InsertionSortTest {
 		int [] expected = { 1, 2, 3, 4, 5 };
 		int [] actual = InsertionSort.sort(array);
 		assertArrayEquals(expected, actual);
+		
+		int [] array2 = {2, 16, 4, 3, 2, 9, 10} ;
+		int [] expected2 = { 2, 2, 3, 4, 9, 10, 16};
+		int [] actual2 = InsertionSort.sort(array2);
+		assertArrayEquals(expected2, actual2);
 	}
 
 }
